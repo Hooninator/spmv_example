6f7e8acf424870192f635cbadf91cb1bb1220dcc
Hooninator <jbellavita@berkeley.edu>
2024-08-21 20:43:33 -0700

    spmv test

--- tests/spmv.cu
+++ tests/spmv.cu
@@ -14,8 +14,99 @@
 
 #include "test_common.h"
 
+using namespace testing;
+
 
 int main(int argc, char ** argv)
 {
+    cusparseHandle_t cusparseHandle;
+    CUSPARSE_CHECK(cusparseCreate(&cusparseHandle));
+
+    size_t m = std::atol(argv[1]);
+    size_t n = std::atol(argv[2]);
+    size_t nnz = std::atol(argv[3]);
+    size_t n_iters = std::atoi(argv[4]);
+
+    float * d_vals, * d_vec, * d_result;
+    uint32_t * d_colinds, * d_rowptrs;
+
+    init_sparse_mat_csr<float, RandomCsrInitializer>
+                        (m, n, nnz, 
+                        &d_vals, &d_colinds, &d_rowptrs, 
+                        RandomCsrInitializer());
+    init_dense_vec(n, &d_vec);
+    init_dense_vec(m, &d_result);
+
+    cusparseDnVecDescr_t x;
+    cusparseDnVecDescr_t y;
+    cusparseSpMatDescr_t A;
+
+    CUSPARSE_CHECK(cusparseCreateDnVec(&x, n, d_vec, CUDA_R_32F));
+    CUSPARSE_CHECK(cusparseCreateDnVec(&y, m, d_result, CUDA_R_32F));
+    CUSPARSE_CHECK(cusparseCreateCsr(&A,
+                                     m, n, nnz,
+                                     d_rowptrs,
+                                     d_colinds,
+                                     d_vals,
+                                     CUSPARSE_INDEX_32I,
+                                     CUSPARSE_INDEX_32I,
+                                     CUSPARSE_INDEX_BASE_ZERO,
+                                     CUDA_R_32F));
+
+    float alpha = 1.0;
+    float beta = 0.0;
+
+    size_t buf_size;
+    void * buf;
+
+    const char * label_cusparse = "SpMV_cusparse";
+
+    start_timer(label_cusparse);
+    for (int i=0; i<n_iters; i++) {
+
+        /* BENCHMARK CUSPARSE */
+        CUSPARSE_CHECK(cusparseSpMV_bufferSize(cusparseHandle,
+                                                CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                                &alpha, A, x,
+                                                &beta, y,
+                                                CUDA_R_32F,
+                                                CUSPARSE_SPMV_ALG_DEFAULT,
+                                                &buf_size));
+
+        CUDA_CHECK(cudaMalloc(&buf, buf_size));
+        CUSPARSE_CHECK(cusparseSpMV(cusparseHandle,
+                                    CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                    &alpha, A, x,
+                                    &beta, y,
+                                    CUDA_R_32F,
+                                    CUSPARSE_SPMV_ALG_DEFAULT,
+                                    buf));
+
+
+
+        CUDA_CHECK(cudaFree(buf));
+    }
+
+    end_timer(label_cusparse);
+    measure_gflops(label_cusparse, 2*nnz*n_iters);
+
+    print_time(label_cusparse);
+    print_gflops(label_cusparse);
+
+    CUSPARSE_CHECK(cusparseDestroySpMat(A));
+    CUSPARSE_CHECK(cusparseDestroyDnVec(x));
+    CUSPARSE_CHECK(cusparseDestroyDnVec(y));
+
+
+    CUDA_CHECK(cudaFree(d_vals));
+    CUDA_CHECK(cudaFree(d_colinds));
+    CUDA_CHECK(cudaFree(d_rowptrs));
+
+    CUDA_CHECK(cudaFree(d_vec));
+    CUDA_CHECK(cudaFree(d_result));
+
+    CUSPARSE_CHECK(cusparseDestroy(cusparseHandle));
+
+
     return 0;
 }
--- tests/test_common.h
+++ tests/test_common.h
@@ -6,6 +6,11 @@
 #include <fstream>
 #include <iostream>
 #include <chrono>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <random>
 
 
 #include <cuda.h>
@@ -15,5 +20,209 @@
 #include <cusparse.h>
 
 #include <cub/cub.cuh>
+
+#define CUDA_CHECK(call) {                                                 \
+    cudaError_t err = call;                                                \
+    if (err != cudaSuccess) {                                              \
+        std::cerr << "CUDA Error: " << cudaGetErrorString(err) << " ("     \
+                  << __FILE__ << ":" << __LINE__ << ")" << std::endl;      \
+        exit(err);                                                         \
+    }                                                                      \
+}
+
+#define CUSPARSE_CHECK(call) do {                                    \
+    cusparseStatus_t err = call;                                     \
+    if (err != CUSPARSE_STATUS_SUCCESS) {                            \
+        fprintf(stderr, "cuSPARSE error in file '%s' in line %i : %s.\n", \
+                __FILE__, __LINE__, cusparseGetErrorString(err));    \
+        exit(EXIT_FAILURE);                                          \
+    }                                                                \
+} while(0)
+
+namespace testing {
+
+
+/***************************************************************************
+ *                                                                         *
+ *                           MATRIX AND VECTOR INITIALIZATION              *
+ *                                                                         *
+ ***************************************************************************/
+std::random_device rd;
+std::mt19937 gen(rd());
+
+template <typename T>
+void init_random_buffer(std::vector<T>& buf,
+                        const int32_t lower, 
+                        const int32_t upper)
+{
+    std::uniform_real_distribution<T> distr(lower, upper);
+    std::generate(buf.begin(), buf.end(), [&](){return distr(gen);}); 
+}
+
+void init_random_buffer(std::vector<uint32_t>& buf,
+                        const int32_t lower, 
+                        const int32_t upper)
+{
+    std::uniform_int_distribution<uint32_t> distr(lower, upper);
+    std::generate(buf.begin(), buf.end(), [&](){return distr(gen);}); 
+}
+
+
+template <typename T>
+void init_dense_mat(const size_t m,
+					const size_t n,
+                    T ** d_vals)
+{
+    std::vector<T> h_vals(m*n);
+    init_random_buffer(h_vals, -1e5, 1e5);
+
+    CUDA_CHECK(cudaMalloc(d_vals, sizeof(T)*m*n));
+    CUDA_CHECK(cudaMemcpy(*d_vals, h_vals.data(), sizeof(T)*m*n, cudaMemcpyHostToDevice));
+}
+
+
+template <typename T>
+void init_dense_vec(const size_t n,
+                    T ** d_vals)
+{
+    std::vector<T> h_vals(n);
+    init_random_buffer(h_vals, -1e5, 1e5);
+
+    CUDA_CHECK(cudaMalloc(d_vals, sizeof(T)*n));
+    CUDA_CHECK(cudaMemcpy(*d_vals, h_vals.data(), sizeof(T)*n, cudaMemcpyHostToDevice));
+}
+
+
+struct RandomCsrInitializer
+{
+    template <typename T>
+    void init(const size_t m,
+             const size_t n,
+             const size_t nnz,
+             std::vector<T>& vals,
+             std::vector<uint32_t>& colinds,
+             std::vector<uint32_t>& rowptrs)
+    {
+        init_random_buffer(vals, -1e5, 1e5);
+        init_random_buffer(colinds, 0, n);
+
+        std::unordered_set<uint32_t> found;
+        found.reserve(m);
+
+        std::uniform_int_distribution<uint32_t> distr(1, n);
+
+        rowptrs[0] = 0;
+        size_t count = 1;
+        while (count < m) {
+            uint32_t idx = distr(gen);
+            if (found.find(idx)==found.end()) {
+                found.insert(idx);
+                rowptrs[count] = idx;
+                count++;
+            }
+        }
+
+        std::sort(rowptrs.begin(), rowptrs.end());
+    }
+
+};
+
+
+
+template <typename T, typename Initializer>
+void init_sparse_mat_csr(const size_t m,
+                         const size_t n,
+                         const size_t nnz,
+                         T ** d_vals,
+                         uint32_t ** d_colinds,
+                         uint32_t ** d_rowptrs,
+                         Initializer initializer)
+{
+    std::vector<T> h_vals(nnz);
+    std::vector<uint32_t> h_colinds(nnz);
+    std::vector<uint32_t> h_rowptrs(m+1);
+
+    initializer.init(m, n, nnz, h_vals, h_colinds, h_rowptrs);
+
+    CUDA_CHECK(cudaMalloc(d_vals, sizeof(T)*nnz));
+    CUDA_CHECK(cudaMalloc(d_colinds, sizeof(T)*nnz));
+    CUDA_CHECK(cudaMalloc(d_rowptrs, sizeof(T)*(m+1)));
+
+    CUDA_CHECK(cudaMemcpy(*d_vals, h_vals.data(), sizeof(T)*nnz,
+                                cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy(*d_colinds, h_colinds.data(), sizeof(uint32_t)*nnz,
+                                cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy(*d_rowptrs, h_rowptrs.data(), sizeof(uint32_t)*(m+1),
+                                cudaMemcpyHostToDevice));
+}
+
+
+/***************************************************************************
+ *                                                                         *
+ *                           PROFILING                                     *
+ *                                                                         *
+ ***************************************************************************/
+
+std::map<std::string, cudaEvent_t> stimes;
+std::map<std::string, cudaEvent_t> etimes;
+
+std::map<std::string, size_t> gflops;
+std::map<std::string, float> times;
+
+void start_timer(const char * label)
+{
+    std::string label_str(label);
+    cudaEvent_t start, end;
+
+    stimes[label_str] = start;
+    etimes[label_str] = end;
+
+    cudaEventCreate(&stimes[label_str]);
+    cudaEventCreate(&etimes[label_str]);
+
+    cudaEventRecord(stimes[label_str]);
+}
+
+
+void end_timer(const char * label)
+{
+    std::string label_str(label);
+
+    cudaEventRecord(etimes[label_str]);
+    cudaEventSynchronize(etimes[label_str]);
+
+    float t = 0;
+    cudaEventElapsedTime(&t, stimes[label_str], etimes[label_str]);
+
+    times[label] = t;
+
+    cudaEventDestroy(stimes[label_str]);
+    cudaEventDestroy(etimes[label_str]);
+}
+
+
+void measure_gflops(const char * label, size_t flops)
+{
+    std::string label_str(label);
+    size_t gflops_per_sec = (flops / 1e9) / (times[label] / 1e3);
+    gflops[label] = gflops_per_sec;
+}
+
+
+void print_time(const char * label)
+{
+    std::string label_str(label);
+    std::cout<<"["<<label_str<<"]: "<<times[label_str]/1e3<<"s"<<std::endl;
+}
+
+
+void print_gflops(const char * label)
+{
+    std::string label_str(label);
+    std::cout<<"["<<label_str<<"]: "<<gflops[label_str]<<" gflops/s"<<std::endl;
+}
+
+
+}
 
 #endif
