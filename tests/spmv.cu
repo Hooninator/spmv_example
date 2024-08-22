c5532b866e475c6cf0c7cff551950138f7f653b4
Hooninator <jbellavita@berkeley.edu>
2024-08-21 17:38:33 -0700

    init

---
+++ tests/spmv.cu
@@ -0,0 +1,21 @@
+
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include <cublas_v2.h>
+#include <cusparse.h>
+
+#include <cub/cub.cuh>
+
+#include "test_common.h"
+
+
+int main(int argc, char ** argv)
+{
+    return 0;
+}
---
+++ tests/test_common.h
@@ -0,0 +1,19 @@
+
+#ifndef TEST_COMMON_H
+#define TEST_COMMON_H
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <chrono>
+
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include <cublas_v2.h>
+#include <cusparse.h>
+
+#include <cub/cub.cuh>
+
+#endif
